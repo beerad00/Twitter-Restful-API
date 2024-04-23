3e2d915c996551c136b93a82a5450e01dd8e197d
Helena Makendengue <HelenaMakens1013@gmail.com>
2024-04-23 17:59:47 -0500

    Update TweetRequestDto.java

--- src/main/java/com/cooksys/social_media_1/dtos/TweetRequestDto.java
+++ src/main/java/com/cooksys/social_media_1/dtos/TweetRequestDto.java
@@ -6,15 +6,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @Data
 public class TweetRequestDto {
-    //User id
-    private int author;
-
-    private Long posted;
-
-    private boolean deleted;
+    
     private String content;
-    //Tweet id
-    private int inReplyTo;
-    //Tweet id
-    private int repostOf;
+
+    private CredentialsRequestDto credentials;
 }
