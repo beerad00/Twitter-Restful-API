1d9a8be8d2fb37475a05b85913734b6410fc75fd
Helena Makendengue <HelenaMakens1013@gmail.com>
2024-04-23 19:23:03 -0500

    Update UserMapper.java

--- src/main/java/com/cooksys/social_media_1/mappers/UserMapper.java
+++ src/main/java/com/cooksys/social_media_1/mappers/UserMapper.java
@@ -2,7 +2,7 @@ package com.cooksys.social_media_1.mappers;
 
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring", uses = { ProfileMapper.class, CredentialsMapper.class, TweetMapper.class })
+@Mapper(componentModel = "spring", uses = { ProfileMapper.class, CredentialsMapper.class })
 public interface UserMapper {
 
 }
