97ddbce97e6bc69bd6270969d459becabc4f7295
Helena Makendengue <HelenaMakens1013@gmail.com>
2024-04-23 19:22:35 -0500

    Update HashtagMapper.java

--- src/main/java/com/cooksys/social_media_1/mappers/HashtagMapper.java
+++ src/main/java/com/cooksys/social_media_1/mappers/HashtagMapper.java
@@ -2,7 +2,7 @@ package com.cooksys.social_media_1.mappers;
 
 import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring", uses = { TweetMapper.class })
+@Mapper(componentModel = "spring")
 public interface HashtagMapper {
 
 }
