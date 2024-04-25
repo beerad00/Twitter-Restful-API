9e8d75323547ad03fcf99cd6ed5789fb75168689
Solfuller25 <fullersolomon5@gmail.com>
2024-04-25 10:40:30 -0400

    Added GET tweets/{id}/replies endpoint
    Might need to confirm deleted replies are being excluded from the results

--- src/main/java/com/cooksys/social_media_1/controllers/TweetController.java
+++ src/main/java/com/cooksys/social_media_1/controllers/TweetController.java
@@ -1,7 +1,14 @@
 package com.cooksys.social_media_1.controllers;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.cooksys.social_media_1.dtos.TweetResponseDto;
+import com.cooksys.social_media_1.services.TweetService;
 
 import lombok.RequiredArgsConstructor;
 
@@ -10,4 +17,10 @@ import lombok.RequiredArgsConstructor;
 @RequestMapping("/tweets")
 public class TweetController {
 
+	private final TweetService tweetService;
+	
+	@GetMapping("/{id}/replies")
+	public List<TweetResponseDto> getTweetReplies(@PathVariable Long id) {
+		return tweetService.getTweetReplies(id);
+	}
 }
